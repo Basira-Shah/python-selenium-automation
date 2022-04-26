8f23d6864f3d7f6eceae15e2842085cb2c1d2b89
Basira <21basira.shah@gmail.com>
2022-04-25 23:00:11 -0400

    Homework 1

--- sample_script.py
+++ sample_script.py
@@ -3,7 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # init driver
-driver = webdriver.Chrome()
+driver = webdriver.Chrome(executable_path="/Users/ahmadshah/Desktop/automation/python-selenium-automation/chromedriver")
 driver.maximize_window()
 
 # open the url
@@ -11,6 +11,8 @@ driver.get('https://www.google.com/')
 
 search = driver.find_element(By.NAME, 'q')
 search.clear()
+
+
 search.send_keys('Dress')
 
 # wait for 4 sec
